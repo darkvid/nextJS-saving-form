ac156400934f7070d80ea7d06bf11932951975bd
David Galisteo Cantero <darkvidvsgalis@gmail.com>
2023-03-28 14:02:12 +0200

    change name form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <h1>Mi formulario de ahorro</h1>
+      <h1>Mi formulario de gastos</h1>
       <SavingForm />
     </main>
   )
